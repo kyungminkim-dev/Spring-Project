5fdfab4eac7b60de8dc5343fc5708b7a7bac08d5
kkim <kkim@172.30.1.22>
2022-11-09 21:24:51 +0900

    Creating Spring Project

---
+++ project1/src/main/java/com/mysite/project1/answer/Answer.java
@@ -0,0 +1,32 @@
+package com.mysite.project1.answer;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.ManyToOne;
+import com.mysite.project1.question.Question;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+    
+    @ManyToOne
+    private Question question;
+
+}
