a34d832cfd2b7bac980d04073efd3e1aa945d47b
kkim <dev.kkim@gmail.com>
2022-11-14 18:38:51 +0900

    Create question detail

--- project1/src/main/java/com/mysite/project1/question/QuestionController.java
+++ project1/src/main/java/com/mysite/project1/question/QuestionController.java
@@ -8,18 +8,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	
-	private final QuestionRepository questionRepository;
+	//private final QuestionRepository questionRepository;
 	
-	@RequestMapping("/question/list")
+	private final QuestionService questionService;
+	
+	@RequestMapping("/list")
     //@ResponseBody
     public String list(Model model) {
-		List<Question> questionList = this.questionRepository.findAll();
+		//List<Question> questionList = this.questionRepository.findAll();
+		
+		List<Question> questionList = this.questionService.getList();
+		
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+	
+	@RequestMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
---
+++ project1/src/main/java/com/mysite/project1/question/QuestionService.java
@@ -0,0 +1,28 @@
+package com.mysite.project1.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import java.util.Optional;
+import com.mysite.project1.DataNotFoundException;
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+	
+	private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+	
+    public Question getQuestion(Integer id) {  
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+}
