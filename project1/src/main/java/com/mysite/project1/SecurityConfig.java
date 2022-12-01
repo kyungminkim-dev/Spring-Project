3d3fde59eb0f409c60fdcc62c1fba3c5aabe02b3
kkim <dev.kkim@gmail.com>
2022-12-01 21:39:35 +0900

    Making Login

--- project1/src/main/java/com/mysite/project1/SecurityConfig.java
+++ project1/src/main/java/com/mysite/project1/SecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -19,5 +22,10 @@ public class SecurityConfig {
 				.headers()
 				.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
 		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
---
+++ project1/src/main/java/com/mysite/project1/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.project1.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UserRepository extends JpaRepository<SiteUser, Long>{
+
+}
